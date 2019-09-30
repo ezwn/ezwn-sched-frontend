e16107fdc862950970d3b765d286ccb49727dc5c
Nicolas ENZWEILER <nicolas@ileotech.com>
2019-09-30 14:15:04 +0200

    Upgraded graphQL definitions

--- src/features/Task.queries.js
+++ src/features/Task.queries.js
@@ -1,20 +1,38 @@
+
 import { gql } from "apollo-boost";
 
 export const findTasksQuery = gql`
   {
     findTasks {
       tasId
-      description
       title
+      description
+      blocked
+      status
     }
   }
 `;
 
-export const createTaskMutation = gql`
-  mutation($title: String!, $description: String) {
-    createTask(title: $title, description: $description) {
-      description
+export const saveTaskMutation = gql`
+  mutation($tasId: Int!, $title: String!, $description: String, $blocked: Boolean!, $status: String!) {
+    saveTask(tasId: $tasId, title: $title, description: $description, blocked: $blocked, status: $status) {
+      tasId
       title
+      description
+      blocked
+      status
+    }
+  }
+`;
+
+export const saveActionMutation = gql`
+  mutation($actId: Int!, $moment: String!, $summary: String, $details: String, $task: Task!) {
+    saveAction(actId: $actId, moment: $moment, summary: $summary, details: $details, task: $task) {
+      actId
+      moment
+      summary
+      details
+      task
     }
   }
 `;
